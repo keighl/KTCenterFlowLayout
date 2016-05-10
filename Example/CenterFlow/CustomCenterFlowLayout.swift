c3c363a92f4047e19ec616acf32be65269122b5a
Rachel Parsons <vc.rachelanne.parsons@lowes.com>
2016-05-10 16:16:40 -0400

    started working on a swift port

---
+++ Example/CenterFlow/CustomCenterFlowLayout.swift
@@ -0,0 +1,22 @@
+//
+//  CustomCenterFlowLayout.swift
+//  CenterFlow
+//
+//  Created by Parsons, Rachelanne - Rachelanne on 5/10/16.
+//  Copyright © 2016 keighl. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CustomerCenterFlowLayout: UICollectionViewFlowLayout {
+    
+    override func layoutAttributesForElementsInRect(rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        
+        let superAttributes = [UICollectionViewLayoutAttributes]()
+        
+        
+        
+        return superAttributes
+    }
+}
